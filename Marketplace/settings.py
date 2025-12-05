fd374e9cdb81d04c4823d8bc7a11b70d923e0aec
Rohan <blckreward02@gmail.com>
2025-12-05 19:56:02 +0200

    settings

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -55,9 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'Marketplace.urls'
 
