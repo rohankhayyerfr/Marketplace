4703d28f3d3af39c56016f46383f10cb4668f4d9
Rohan <blckreward02@gmail.com>
2025-12-05 20:34:18 +0200

    WHITE NOISE ADDED wsgi

--- Marketplace/wsgi.py
+++ Marketplace/wsgi.py
@@ -2,15 +2,19 @@
 WSGI config for Marketplace project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+# مسیر پروژه
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Marketplace.settings')
 
 application = get_wsgi_application()
+
+# WhiteNoise برای سرو فایل‌های استاتیک
+application = WhiteNoise(application, root=str(BASE_DIR / "staticfiles"), prefix="static/")
