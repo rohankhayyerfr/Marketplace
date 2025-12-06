d6bb4697f9849f46aa3d4e1601c620a18a75e343
Rohan <blckreward02@gmail.com>
2025-12-06 17:08:39 +0200

    fixing part 2

--- Marketplace/urls.py
+++ Marketplace/urls.py
@@ -28,4 +28,6 @@ path('admin/', admin.site.urls),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
