ecd92ed63a138676506fcf6dd9c417b714fe140e
Rohan <blckreward02@gmail.com>
2025-12-06 12:07:18 +0200

    media root

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -148,8 +148,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
+MEDIA_ROOT = '/data/media'  # زیرmount path باشه تا persist بشه
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
