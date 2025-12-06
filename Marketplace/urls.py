f9494f1760a0add3044b23322b10cbd3b9ffcea3
Rohan <blckreward02@gmail.com>
2025-12-06 12:14:39 +0200

    media root

--- Marketplace/urls.py
+++ Marketplace/urls.py
@@ -29,5 +29,6 @@ path('admin/', admin.site.urls),
 ]
 if not settings.DEBUG:
     urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
+    
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
