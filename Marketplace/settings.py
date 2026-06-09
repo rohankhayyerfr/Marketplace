3bd58a909289451677e420161172ee7b175c49bd
Rohan <blckreward02@gmail.com>
2026-06-09 22:20:37 +0200

    An update

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -85,23 +85,12 @@ WSGI_APPLICATION = 'Marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get(
-                'DATABASE_URL',
-                'postgresql://mydatabase_f3vw_user:sLRo6ziBLBpFjhnl6Y3CA31rvReW41AA@dpg-d4pcj2er433s73ekjieg-a.frankfurt-postgres.render.com/mydatabase_f3vw'
-            )
-        )
-    }
-
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
